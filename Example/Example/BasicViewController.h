1dc1a4c2e7004c626ff4c3499b396474707af30f
Kenneth Yip <kenneth.yip@qochealth.com>
2017-01-30 15:35:36 -0500

    updating library UI for PATH project

--- Example/Example/BasicViewController.h
+++ Example/Example/BasicViewController.h
@@ -7,7 +7,7 @@
 
 #import <UIKit/UIKit.h>
 
-#import <JTCalendar/JTCalendar.h>
+#import <QoCCalendar/JTCalendar.h>
 
 @interface BasicViewController : UIViewController<JTCalendarDelegate>
 
---
+++ Example/Pods/Target Support Files/QoCCalendar/QoCCalendar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QoCCalendar : NSObject
+@end
+@implementation PodsDummy_QoCCalendar
+@end
